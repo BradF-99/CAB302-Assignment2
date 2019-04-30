a364438cc39a53ccee26ddb9942b9e151d715960
Brad Fuller <bfuller@everware.io>
2019-04-30 15:24:33 +1000

    Created IntelliJ Project

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # CAB302-Assignment2
-Assignment 2 (Vector Design Tool) for CAB302, Semester 1, 2019.
+
+
+
+Assignment 2 (Vector Design Tool) for CAB302, Semester 1, 2019.   
+Built to Assignment Specification v1.0.
+
+## Authors
+
+* Alex Butler (Assasindie)
+* Brad Fuller (BradF-99)  
---
+++ test/ExampleTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/*
+ * Example Test
+ * This is here to activate TravisCI. It should be removed before submission.
+ */
+public class ExampleTest {
+    @Test
+    public void testSomething() {
+        int testSum = 3+6+9;
+        assertEquals(18,testSum);
+    }
+}
