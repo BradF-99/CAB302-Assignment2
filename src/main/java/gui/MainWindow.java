98814da3b568390888147db99cf611d62c977755
Assasindie <xmanbutler@gmail.com>
2019-05-03 15:57:49 +1000

    Added basic mouse handling
    Displays x,y position of the location where the mouse was released and adds it as a new label.

--- src/main/java/gui/MainWindow.java
+++ src/main/java/gui/MainWindow.java
@@ -1,20 +1,23 @@
 package main.java.gui;
 
-import java.awt.Dimension;
-import java.awt.Point;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.*;
 
 
 public class MainWindow {
+    JFrame frame;
     /**
      * This is just an example thread-safe GUI based off the example from the lecture.
      */
-    private static void buildGUI() {
-        JFrame frame = new JFrame("Hello World");
+    private void buildGUI() {
+        frame = new JFrame("Hello World");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JLabel label = new JLabel(main.java.Main.exampleFunc(true),SwingConstants.CENTER);
+        JLabel label = new JLabel(main.java.Main.exampleFunc(true),SwingConstants.LEFT);
+        label.addMouseListener((new MyMouseAdapter()));
         frame.getContentPane().add(label);
 
         frame.setPreferredSize(new Dimension(500, 250));
@@ -23,7 +26,27 @@ public class MainWindow {
         frame.setVisible(true);
     }
 
-    public static void showGUI() {
+    //inner MouseAdapter class for handling mouse inputs, removes the need for having every event implemented
+    class MyMouseAdapter extends MouseAdapter {
+        private java.awt.Point startPoint;
+        private java.awt.Point endPoint;
+
+        public void mousePressed(MouseEvent e) {
+            System.out.println(e.getPoint());
+            startPoint = e.getPoint();
+        }
+        public void mouseReleased(MouseEvent e) {
+            System.out.println(e.getPoint());
+            endPoint = e.getPoint();
+            JLabel mousePoints = new JLabel(endPoint.toString(),SwingConstants.CENTER);
+            frame.getContentPane().add(mousePoints);
+            frame.getContentPane().revalidate();
+        }
+
+
+    }
+
+    public void showGUI() {
         javax.swing.SwingUtilities.invokeLater(() -> {
             buildGUI();
         });
