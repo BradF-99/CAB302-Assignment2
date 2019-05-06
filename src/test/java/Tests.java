4d23b3e8dd267a0aacaada345265ade9344f77e9
Brad Fuller <bfuller@everware.io>
2019-05-06 14:04:25 +1000

    Added descriptions of each test for File Handling

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,20 +1,46 @@
 package test.java;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import main.java.*;
 
 public class Tests {
     @Test
     public void testExample() {
         int testSum = 3+6+9;
-        assertEquals(18,testSum);
+        Assertions.assertEquals(18,testSum);
     }
     @Test
     public void testMainExampleTrue (){
-        assertEquals(main.java.Main.exampleFunc(true),"Hi!");
+        Assertions.assertEquals(Main.exampleFunc(true),"Hi!");
     }
     @Test
     public void testMainExampleFalse (){
-        assertEquals(main.java.Main.exampleFunc(false),"Hello!");
+        Assertions.assertEquals(Main.exampleFunc(false),"Hello!");
     }
+
+    /*
+     * File Handling Tests
+     *
+     * Things we need to test for during read:
+     * - Testing read of a valid VEC file
+     * - Testing read of an invalid VEC file
+     * - Testing read of a file that is partially valid (has some incorrect arguments)
+     * - Testing read of a non-VEC file
+     * - Testing read of a non-existent file
+     * - Testing read of a file that has been deleted or has become inaccessible during processing
+     * - Testing read of a file that we do not have permissions to open
+     * - Testing read of a file that is wrongly encoded
+     *
+     * Things we need to test for during write:
+     * - Testing write of a VEC file
+     * - Verifying a successful write of a VEC file (using read)
+     * - Testing write of a VEC file to a read only folder
+     * - Testing write of a VEC file to a place where we do not have access (OS permissions)
+     * - Testing write of a VEC file where the media becomes inaccessible during write
+     * - Testing write of a very detailed and/or large file
+     * - Testing overwriting an existing file
+     * - Testing overwriting a file that we do not have permission to overwrite
+     */
+
 }
