6de9dbe70cc16e75488a717c78b576dbda016fcd
Brad Fuller <37586752+BradF-99@users.noreply.github.com>
2019-06-02 17:39:52 +1000

    Implemented the FileWrite class

--- src/main/java/filehandler/FileWrite.java
+++ src/main/java/filehandler/FileWrite.java
@@ -1,14 +1,31 @@
 package main.java.filehandler;
 
-import java.io.File;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 
 public class FileWrite {
 
-    public void writeFile(List<String> arguments, String fileName) throws IOException {
-        System.out.println(fileName);
-
-        //File file = new File(fileName);
+    /**
+     * writeFile takes the output from the GUI and writes it to a file.
+     * Authors note: I was originally going to use FileChannel and RandomAccessFile
+     * but it seems a bit overkill for what we need. This BufferedWriter should do
+     * just fine.
+     * We wrap the FileWriter in a BufferedWriter for performance, and also to gain
+     * access to the newLine() method. All of our validation is done for us, so all
+     * we have to do is write what we're given to the file.
+     *
+     * @param arguments a list of arguments given from the GUI as an ArrayList.
+     * @param filePath the complete file path to write to.
+     * @throws IOException
+     */
+    public void writeFile(List<String> arguments, String filePath) throws IOException {
+        BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, false));
+        for (String arg: arguments) {
+            writer.write(arg);
+            writer.newLine();
+        }
+        writer.close();
     }
 }
