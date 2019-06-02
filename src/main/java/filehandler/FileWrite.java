8a4ddaee39c38e37dd3fd0611dbde25f21be1f88
Brad Fuller <37586752+BradF-99@users.noreply.github.com>
2019-06-02 17:05:15 +1000

    Framework for file writer class

--- src/main/java/filehandler/FileWrite.java
+++ src/main/java/filehandler/FileWrite.java
@@ -1,4 +1,14 @@
 package main.java.filehandler;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
 public class FileWrite {
+
+    public void writeFile(List<String> arguments, String fileName) throws IOException {
+        System.out.println(fileName);
+
+        //File file = new File(fileName);
+    }
 }
